9f2bc75b9005907584f847b0107a2332726a7249
paras-j <59125176+paras-j@users.noreply.github.com>
2022-03-18 01:24:06 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,9 +13,7 @@ st.title("Question Answering Webapp")
 st.text("What would you like to know today?")
 
 @st.cache(allow_output_mutation=True)
-
 with st.spinner ('Loading Model into Memory....'):
-    #retreiver = DensePassageRetriever.load(load_dir='/content/drive/My Drive/kiku/got_retreiver', document_store=document_store)
     retriever = DensePassageRetriever(document_store=document_store, query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki", passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",)
     generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
     pipe = GenerativeQAPipeline(generator, retriever)  
@@ -24,8 +22,11 @@ text = st.text_input('Enter your questions here....') # no input required
 if text:
     st.write("Response:")
     with st.spinner('Searching for answers....'):
-        prediction = pipe.run(query=text, params={"Retriever": {"top_k": 2}})
+        prediction = pipe.run(query=text, params={"Retriever": {"top_k": 3}})
         st.write('answer: {}'.format(prediction[0]))
 #        st.write('title: {}'.format(prediction[1]))
 #        st.write('paragraph: {}'.format(prediction[2]))
-    st.write("")
+    st.write("")    
+    
+    
+    
