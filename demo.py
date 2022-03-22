8da5cc82e654000814be8d88a17c187ffb24e345
paras-j <59125176+paras-j@users.noreply.github.com>
2022-03-22 00:53:01 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,46 +1,48 @@
 import streamlit as st
-#from haystack.utils import convert_files_to_dicts, fetch_archive_from_http, clean_wiki_text
-from haystack.nodes import Seq2SeqGenerator
 from haystack.document_stores import FAISSDocumentStore
 from haystack.nodes import DensePassageRetriever
+from haystack.nodes import Seq2SeqGenerator
 from haystack.pipelines import GenerativeQAPipeline
 
-@st.cache(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def get_retriever():
     document_store = FAISSDocumentStore.load("haystack_got_faiss_1")
+    st.write("I got document_store")
     retriever = DensePassageRetriever(document_store=document_store, query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki", passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",)
+    st.write("I got retriever")
     return retriever
 
-@st.cache(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def get_generator():
+    st.write("I am inside retriever")
     generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
+    st.write("I got generator")
     return generator
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("Question Answering Webapp")
 st.text("What would you like to know today?")
 
-#with st.spinner ('Loading Model into Memory....'):
-retriever = get_retriever()
-generator = get_generator()
-pipe = GenerativeQAPipeline(generator, retriever)  
+with st.spinner ('Loading Model into Memory....'):
+    retriever = get_retriever()
+    generator = get_generator()
+    pipe = GenerativeQAPipeline(generator, retriever)  
 
 text = st.text_input('Enter your questions here....') # no input required
 if text:
     st.write("Response:")
     with st.spinner('Searching for answers....'):
-        prediction = pipe.run(query=text, params={"Retriever": {"top_k": 3}})
+        st.write("I'm in spinner")
+        prediction = pipe.run(query=text, params={"Retriever": {"top_k": 1}})
 #        st.write('answer: {}'.format(prediction[0]))
 #        st.write('title: {}'.format(prediction[1]))
 #        st.write('paragraph: {}'.format(prediction[2]))
     st.write("")
 
 
-    
-    
-    
-    
-    
+
+
+
 # #from haystack.utils import convert_files_to_dicts, fetch_archive_from_http, clean_wiki_text
 # import streamlit as st
 # from haystack.document_stores import FAISSDocumentStore
