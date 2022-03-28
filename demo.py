204127222212f73e17cc566057a098ed1f278459
paras-j <59125176+paras-j@users.noreply.github.com>
2022-03-28 13:03:16 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from haystack.document_stores import FAISSDocumentStore
 from haystack.nodes import DensePassageRetriever
-from haystack.nodes import Seq2SeqGenerator
-from haystack.pipelines import GenerativeQAPipeline
+from haystack.nodes import FARMReader, TransformersReader
+from haystack.pipelines import ExtractiveQAPipeline
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def get_retriever():
@@ -13,12 +13,11 @@ def get_retriever():
     return retriever
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def get_generator():
-    st.write("I am inside generator")
-    #generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
-    generator = Seq2SeqGenerator(model_name_or_path="yjernite/bart_eli5")
-    st.write("I got generator")
-    return generator
+def get_reader():
+    st.write("I am inside reader")
+    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
+    st.write("I got reader")
+    return reader
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("Question Answering Webapp")
@@ -26,22 +25,63 @@ st.text("What would you like to know today?")
 
 with st.spinner ('Loading Model into Memory....'):
     retriever = get_retriever()
-    generator = get_generator()
-    pipe = GenerativeQAPipeline(generator, retriever)  
+    generator = get_reader()
+    pipe = ExtractiveQAPipeline(reader, retriever)  
 
 text = st.text_input('Enter your questions here....')
 if text:
     st.write("Response:")
     with st.spinner('Searching for answers....'):
         st.write("I'm in spinner")
-        prediction = pipe.run(query=text, params={"Retriever": {"top_k": 1}})
-#        st.write('answer: {}'.format(prediction[0]))
-#        st.write('title: {}'.format(prediction[1]))
-#        st.write('paragraph: {}'.format(prediction[2]))
+        prediction = pipe.run(query=text, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 1}})
+        st.write('answer: {}'.format(prediction['answers'][0].answer))
     st.write("")
 
 
 
+    
+    
+# import streamlit as st
+# from haystack.document_stores import FAISSDocumentStore
+# from haystack.nodes import DensePassageRetriever
+# from haystack.nodes import Seq2SeqGenerator
+# from haystack.pipelines import GenerativeQAPipeline
+
+# @st.cache(allow_output_mutation=True, suppress_st_warning=True)
+# def get_retriever():
+#     document_store = FAISSDocumentStore.load("haystack_got_faiss_1")
+#     st.write("I got document_store")
+#     retriever = DensePassageRetriever(document_store=document_store, query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki", passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",)
+#     st.write("I got retriever")
+#     return retriever
+
+# @st.cache(allow_output_mutation=True, suppress_st_warning=True)
+# def get_generator():
+#     st.write("I am inside generator")
+#     #generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
+#     generator = Seq2SeqGenerator(model_name_or_path="yjernite/bart_eli5")
+#     st.write("I got generator")
+#     return generator
+
+# st.set_option('deprecation.showfileUploaderEncoding', False)
+# st.title("Question Answering Webapp")
+# st.text("What would you like to know today?")
+
+# with st.spinner ('Loading Model into Memory....'):
+#     retriever = get_retriever()
+#     generator = get_generator()
+#     pipe = GenerativeQAPipeline(generator, retriever)  
+
+# text = st.text_input('Enter your questions here....')
+# if text:
+#     st.write("Response:")
+#     with st.spinner('Searching for answers....'):
+#         st.write("I'm in spinner")
+#         prediction = pipe.run(query=text, params={"Retriever": {"top_k": 1}})
+# #        st.write('answer: {}'.format(prediction[0]))
+# #        st.write('title: {}'.format(prediction[1]))
+# #        st.write('paragraph: {}'.format(prediction[2]))
+#     st.write("")    
 
 
 # #from haystack.utils import convert_files_to_dicts, fetch_archive_from_http, clean_wiki_text
